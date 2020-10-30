024aa21fba034267fdf278f0bfada7b0586c58b1
uurl <raulestradaa@gmail.com>
2020-10-30 03:33:21 -0600

    First commit

---
+++ src/test/java/acceptance/AcceptanceTest.java
@@ -0,0 +1,13 @@
+package acceptance;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Acceptance Test
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:acceptance")
+public class AcceptanceTest {
+}
---
+++ src/test/java/smoke/SmokeTest.java
@@ -0,0 +1,13 @@
+package smoke;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Smoke Test
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:smoke")
+public class SmokeTest {
+}
